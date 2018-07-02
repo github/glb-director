f8e56232986815539838398e6547dc9bb9abc71c
Theo Julienne <theojulienne@github.com>
2018-07-02 10:21:30 +1000

    glb-redirect: Apply GPL v2 and add README and license discussion.

--- src/glb-redirect/ipt_glbredirect.h
+++ src/glb-redirect/ipt_glbredirect.h
@@ -1,3 +1,24 @@
+/*
+ * libxt_GLBREDIRECT: iptables target for GLB proxy alternate fallback
+ *
+ * Copyright (c) 2018 GitHub.
+ *
+ * This file is part of the `glb-redirect` iptables module.
+ *
+ * glb-redirect is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * glb-redirect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glb-redirect.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef __IPT_GLBREDIRECT_MATCH__
 #define __IPT_GLBREDIRECT_MATCH__
 
