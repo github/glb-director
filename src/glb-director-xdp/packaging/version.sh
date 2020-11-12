5f26290419668bc42ad83b74b1c2cf964441b94d
Theo Julienne <theojulienne@github.com>
2020-11-12 13:20:07 +1100

    Bump xdp-root-shim package for systemd network dependency changes.

--- src/glb-director-xdp/packaging/version.sh
+++ src/glb-director-xdp/packaging/version.sh
@@ -3,4 +3,4 @@
 # keep these in lock step for now to make it easier to reason about
 GLB_DIRECTOR_XDP_VERSION="${GLB_DIRECTOR_VERSION}"
 
-XDP_ROOT_SHIM_VERSION="0.0.1"
+XDP_ROOT_SHIM_VERSION="0.0.2"
